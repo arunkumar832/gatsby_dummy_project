79d7b0448c5a2a7fb32dc1e16925c528b2d765e1
arunkumar832 <jntu784@gmail.com>
2021-12-30 03:21:38 -0500

    Added Header and Body container and some other features

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
 import * as React from "react"
 import styled from "styled-components"
 import { Link } from "gatsby"
+import { FaBars } from "react-icons/fa"
+import { menuData } from "../data/MenuData"
+import { Button } from "./Button"
 
 const Header = () => {
   return (
     <Nav>
       <NavLink to="/">SLACKTEAM</NavLink>
+      <Bars />
+      <NavMenu>
+          { menuData.map((item, index) => (
+              <NavLink to={item.link} key={index}>
+                  {item.title}
+              </NavLink>
+          ))}
+      </NavMenu>
+      <NavBtn>
+        <Button primary="true" round="true" to="/something">
+          Do Something</Button>
+      </NavBtn>
     </Nav>
   )
 }
@@ -13,7 +28,7 @@ const Header = () => {
 export default Header
 
 const Nav = styled.nav`
-  background: red;
+  background: transparent;
   height: 88px;
   display: flex;
   justify-content: space-between;
@@ -29,4 +44,37 @@ const NavLink = styled(Link)`
   padding: 0 1rem;
   height: 100%;
   cursor: pointer;
+`
+const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+
+  @media screen and (max-width: 768px){
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+
+
+  @media screen and (max-width: 768px){
+    display: none;
+  }
+`
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px){
+    display: none;
+  }
 `
